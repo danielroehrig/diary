5696093ebfae7e4daaaad86267beb386bc57e7b9
Daniel Röhrig <mail@danielroehrig.de>
2022-01-10 21:50:53 +0100

    Getting comfortable with this

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,31 @@
 <template>
-	<div>I am an app</div>
+	<Content :class="{'icon-loading': loading}" app-name="diary">
+		<AppNavigation>
+			<template #default>
+				<AppNavigationNew
+					:text="Whatever" />
+			</template>
+		</AppNavigation>
+		<AppContent>
+			Well well well
+		</AppContent>
+	</Content>
 </template>
 
 <script>
+import { Content, AppNavigation, AppNavigationNew, AppContent } from '@nextcloud/vue'
 export default {
 	name: 'App',
+	components: {
+		Content,
+		AppNavigation,
+		AppNavigationNew,
+		AppContent,
+	},
+	data() {
+		return {
+			loading: false,
+		}
+	},
 }
 </script>
--- templates/index.php
+++ templates/index.php
@@ -1,7 +1,3 @@
-
 <div id="vue-content">
-This is message {{ message }}
-<p>Here is more text</p>
-<todo-item>test</todo-item>
 
 </div>
