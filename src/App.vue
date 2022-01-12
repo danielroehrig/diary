b32b3f081acd81260b97aedbc4b64a0738f03f03
Daniel Röhrig <mail@danielroehrig.de>
2022-01-12 22:38:50 +0100

    Move editor to own component

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,25 @@
 <template>
 	<Content :class="{'icon-loading': loading}" app-name="diary">
-		<AppNavigation>
-			<template #default>
-				<AppNavigationNew
-					:text="Whatever" />
-			</template>
-		</AppNavigation>
-		<AppContent>
-			<div class="editor">
-				<textarea />
-			</div>
-		</AppContent>
+		<AppNavigation />
+		<Editor />
 	</Content>
 </template>
 
 <script>
-import { Content, AppNavigation, AppNavigationNew, AppContent } from '@nextcloud/vue'
-import EasyMDE from 'easymde'
+import { AppNavigation, Content } from '@nextcloud/vue'
+import Editor from './Editor'
+
 export default {
 	name: 'App',
 	components: {
+		Editor,
 		Content,
 		AppNavigation,
-		AppNavigationNew,
-		AppContent,
 	},
 	data() {
 		return {
 			loading: false,
-			editor: null,
 		}
-	},
-	mounted() {
-		this.editor = new EasyMDE()
 	},
 }
 </script>
---
+++ src/Editor.vue
@@ -0,0 +1,42 @@
+<template>
+	<AppContent>
+		<div class="editor">
+			<textarea id="diary-editor" />
+		</div>
+	</AppContent>
+</template>
+<script>
+import { AppContent } from '@nextcloud/vue'
+import EasyMDE from 'easymde'
+
+export default {
+	name: 'Editor',
+	components: {
+		AppContent,
+	},
+	data() {
+		return {
+			editor: null,
+		}
+	},
+	mounted() {
+		this.editor = new EasyMDE({
+			element: document.getElementById('diary-editor'),
+			autoDownloadFontAwesome: false,
+			autofocus: true,
+			forceSync: true,
+			spellChecker: false,
+			nativeSpellcheck: false,
+		})
+	},
+}
+</script>
+<style lang="css">
+@import '@fortawesome/fontawesome-free/css/all.min.css';
+@import '~easymde/dist/easymde.min.css';
+
+.editor {
+	padding-left: 3em;
+	padding-top: 3em;
+}
+</style>
