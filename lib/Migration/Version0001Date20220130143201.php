ff33a93ba97980fbb9504c39ea4cea054f543fac
Daniel Röhrig <mail@danielroehrig.de>
2022-01-31 23:10:55 +0100

    [B] Add migration to create diary table

---
+++ lib/Migration/Version0001Date20220130143201.php
@@ -0,0 +1,34 @@
+<?php
+namespace OCA\Diary\Migration;
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\Migration\IOutput;
+use OCP\Migration\SimpleMigrationStep;
+
+class Version0001Date20220130143201 extends SimpleMigrationStep
+{
+    /**
+     * @param IOutput $output
+     * @param Closure $schemaClosure
+     * @param array $options
+     * @return ISchemaWrapper
+     * @throws \Doctrine\DBAL\Schema\SchemaException
+     */
+    public function changeSchema(IOutput $output, Closure $schemaClosure, array $options)
+    {
+        /** @var ISchemaWrapper $schema */
+        $schema = $schemaClosure();
+
+        if(!$schema->hasTable('diary')){
+            $table = $schema->createTable('diary');
+            $table->addColumn('uid','string', ['length'=>64]);
+            $table->addColumn('entry_date','string', ['length'=>10]);
+            $table->addColumn('entry_content','text', ['notnull'=>false]);
+            $table->setPrimaryKey(['uid', 'entry_date'],'diary_user_id_date');
+            $table->addForeignKeyConstraint('users',['uid'], ['uid'],[],'foreign_user_id');
+        }
+
+        return $schema;
+    }
+
+}
