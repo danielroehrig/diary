fe677a157fdde2973ec98a54222c369676575cb6
Daniel Röhrig <mail@danielroehrig.de>
2022-03-20 20:42:54 +0100

    [B#18] Write first controller test
    Signed-off-by: Daniel Röhrig <mail@danielroehrig.de>

--- tests/Unit/Controller/PageControllerTest.php
+++ tests/Unit/Controller/PageControllerTest.php
@@ -2,30 +2,99 @@
 
 namespace OCA\Diary\Tests\Unit\Controller;
 
-use PHPUnit_Framework_TestCase;
+use OCA\Diary\Db\Entry;
+use OCA\Diary\Db\EntryMapper;
+use OCP\AppFramework\Db\DoesNotExistException;
+use OCP\AppFramework\Http;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
 
 use OCP\AppFramework\Http\TemplateResponse;
 
 use OCA\Diary\Controller\PageController;
 
 
-class PageControllerTest extends PHPUnit_Framework_TestCase {
-	private $controller;
-	private $userId = 'john';
+class PageControllerTest extends TestCase
+{
+    /** @var PageController */
+    private $controller;
+    private $userId = 'john';
+    /** @var EntryMapper|MockObject */
+    private $mapper;
 
-	public function setUp() {
-		$request = $this->getMockBuilder('OCP\IRequest')->getMock();
+    public function setUp(): void
+    {
+        $request = $this->getMockBuilder('OCP\IRequest')->getMock();
+        $this->mapper = $this->getMockBuilder(EntryMapper::class)
+            ->disableOriginalConstructor()
+            ->getMock();
 
-		$this->controller = new PageController(
-			'diary', $request, $this->userId
-		);
-	}
+        $this->controller = new PageController(
+            'diary', $request, $this->userId, $this->mapper
+        );
+    }
 
-	public function testIndex() {
-		$result = $this->controller->index();
+    public function testIndex()
+    {
+        $result = $this->controller->index();
 
-		$this->assertEquals('index', $result->getTemplateName());
-		$this->assertTrue($result instanceof TemplateResponse);
-	}
+        $this->assertEquals('index', $result->getTemplateName());
+        $this->assertTrue($result instanceof TemplateResponse);
+    }
+
+    public function testGetEntry()
+    {
+        $entryDate = "2022-08-07";
+        $entryContent = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam";
+        $entry = $this->createMockEntry($entryDate, $this->userId, $entryContent);
+        $this->mapper->expects($this->once())
+            ->method('find')
+            ->with($this->equalTo($this->userId),
+                $this->equalTo($entryDate))
+            ->will($this->returnValue($entry));
+        $result = $this->controller->getEntry($entryDate);
+        $this->assertEquals(Http::STATUS_OK, $result->getStatus());
+        $this->assertEquals($entry, $result->getData());
+    }
+
+    public function testNotFound()
+    {
+        $entryDate = "2022-08-07";
+        $this->mapper->expects($this->once())
+            ->method('find')
+            ->with($this->equalTo($this->userId),
+                $this->equalTo($entryDate))
+            ->will($this->throwException(new DoesNotExistException("Id not found")));
+        $result = $this->controller->getEntry($entryDate);
+        $this->assertEquals(Http::STATUS_OK, $result->getStatus());
+        $this->assertEquals(["isEmpty" => true], $result->getData());
+    }
+
+    /**
+     * Create an Entry element
+     * @param string $date
+     * @param string $userId
+     * @param string $content
+     * @return Entry
+     */
+    private function createMockEntry(string $date, string $userId, string $content): Entry
+    {
+        $entry = new Entry();
+        $reflectedEntry = new \ReflectionClass(Entry::class);
+        $reflectedDate = $reflectedEntry->getProperty('entryDate');
+        $reflectedDate->setAccessible(true);
+        $reflectedDate->setValue($entry, $date);
+        $reflectedUserId = $reflectedEntry->getProperty('uid');
+        $reflectedUserId->setAccessible(true);
+        $reflectedUserId->setValue($entry, $userId);
+        $reflectedContent = $reflectedEntry->getProperty('entryContent');
+        $reflectedContent->setAccessible(true);
+        $reflectedContent->setValue($entry, $content);
+        $reflectedId = $reflectedEntry->getProperty('id');
+        $reflectedId->setAccessible(true);
+        $reflectedId->setValue($entry, $userId . $date);
+        return $entry;
+
+    }
 
 }
