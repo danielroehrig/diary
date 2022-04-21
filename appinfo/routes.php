768101af19f69d9909ce34a0d3585b0ca051a3ed
Daniel Röhrig <mail@danielroehrig.de>
2022-04-21 22:30:49 +0200

    [B#3] Add route for the PDF exporter
    Signed-off-by: Daniel Röhrig <mail@danielroehrig.de>

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -14,5 +14,6 @@ return [
         ['name' => 'page#get_entry', 'url' => '/entry/{date}', 'verb' => 'GET'],
         ['name' => 'page#update_entry', 'url' => '/entry/{date}', 'verb' => 'PUT'],
         ['name' => 'export#get_markdown', 'url' => '/export/markdown', 'verb' => 'GET'],
+        ['name' => 'export#get_pdf', 'url' => '/export/pdf', 'verb' => 'GET'],
     ]
 ];
