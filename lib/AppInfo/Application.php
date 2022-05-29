f66bd346064319f96ec3caa9a538dd6bb7520f74
Daniel Röhrig <mail@danielroehrig.de>
2022-05-29 22:31:50 +0200

    [B#25] Delete all entries for a user when that user is deleted
    Signed-off-by: Daniel Röhrig <mail@danielroehrig.de>

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -2,14 +2,15 @@
 
 namespace OCA\Diary\AppInfo;
 
+use OCA\Diary\Listener\UserDeletedListener;
 use OCP\AppFramework\App;
 use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
+use OCP\User\Events\UserDeletedEvent;
 
 class Application extends App implements IBootstrap
 {
-
     /** @var string */
     public const APP_ID = 'diary';
 
@@ -20,7 +21,8 @@ class Application extends App implements IBootstrap
 
     public function register(IRegistrationContext $context): void
     {
-        include_once __DIR__ . '/../../vendor/autoload.php';
+        include_once __DIR__.'/../../vendor/autoload.php';//TODO Check if this is needed at all
+        $context->registerEventListener(UserDeletedEvent::class, UserDeletedListener::class);
     }
 
     public function boot(IBootContext $context): void
